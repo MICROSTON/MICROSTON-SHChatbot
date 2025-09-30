916050018660a7674704b682b63be56a56f9fdbc
Jaehun9971 <jaehun9971@gmail.com>
2025-09-30 14:13:34 +0900

    Delete chatbot directory

--- chatbot/src/main/java/com/shinhan/chatbot/dto/BenefitSearchRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.shinhan.chatbot.dto;
-
-import lombok.Data;
-
-@Data
-public class BenefitSearchRequestDto {
-    private int ageGroupNum;
-    private Long categoryNum;
-}
--- chatbot/src/main/java/com/shinhan/chatbot/repository/AgeGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shinhan.chatbot.repository;
-
-import com.shinhan.chatbot.DB.AgeGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgeGroupRepository extends JpaRepository<AgeGroup, Integer> {
-}
